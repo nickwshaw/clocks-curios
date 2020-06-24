469afd0e3a95c6151ca9c67ffde6b6ad1c3d7347
Nick Shaw <nickwshaw@hotmail.com>
2020-06-24 15:44:42 +0100

    Only run migrations on non prod

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -16,8 +16,22 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
 		composer install --prefer-dist --no-progress --no-suggest --no-interaction
 		bin/console assets:install --no-interaction
 		bin/console sylius:theme:assets:install public --no-interaction
+	else
+	  echo 'Production no composer install. Its all here already baby'
 	fi
 
+	if [ "$APP_ENV" != 'prod' ]; then
+    until bin/console doctrine:query:sql "select 1" >/dev/null 2>&1; do
+        (>&2 echo "Waiting for MySQL to be ready...")
+      sleep 1
+    done
+	fi
+
+	if [ "$APP_ENV" != 'prod' ]; then
+    if [ "$(ls -A src/Migrations/*.php 2> /dev/null)" ]; then
+      bin/console doctrine:migrations:migrate --no-interaction
+    fi
+  fi
 fi
 
 exec docker-php-entrypoint "$@"
